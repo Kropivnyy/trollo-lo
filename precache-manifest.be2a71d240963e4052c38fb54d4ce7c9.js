6b5b330a8f4bf2e491e627f5795645e688c39057
Kropivnyy <55694059+Kropivnyy@users.noreply.github.com>
2020-05-31 05:49:04 +0300

    Deploy

---
+++ precache-manifest.be2a71d240963e4052c38fb54d4ce7c9.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "489b268d56d326ac31aa",
+    "url": "/trollo-lo/css/app.64d7d4cb.css"
+  },
+  {
+    "revision": "4c69654924d9f2911d428f202adc425f",
+    "url": "/trollo-lo/img/primary_bg.jpg"
+  },
+  {
+    "revision": "371166f180ff89d05997a6108b38c3b1",
+    "url": "/trollo-lo/img/sprite.svg"
+  },
+  {
+    "revision": "518988922deeb4718649181f3d8e2dd2",
+    "url": "/trollo-lo/index.html"
+  },
+  {
+    "revision": "489b268d56d326ac31aa",
+    "url": "/trollo-lo/js/app.0a2a66f4.js"
+  },
+  {
+    "revision": "6dc1544923b11e3de60b",
+    "url": "/trollo-lo/js/chunk-2d0a4b7b.2e2fff57.js"
+  },
+  {
+    "revision": "97a34c164f64e8c0b882",
+    "url": "/trollo-lo/js/chunk-a1a15f6a.9cb15891.js"
+  },
+  {
+    "revision": "f34f482a675296ac38c2",
+    "url": "/trollo-lo/js/chunk-vendors.2383ca12.js"
+  },
+  {
+    "revision": "5b60d7ad99b4941ccb22f4c878826486",
+    "url": "/trollo-lo/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/trollo-lo/robots.txt"
+  }
+]);
